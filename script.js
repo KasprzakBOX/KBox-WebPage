7ec22a8fc9ba76d8ccf69e4c5108bb350e6f8297
Bergerihnio <109215616+Bergerihnio@users.noreply.github.com>
2024-03-26 16:48:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
+var lastActiveInput = null;
+
 function addToInput(value) {
-    var phoneNumberInput = document.getElementById('phone_number');
+    var phoneNumberInput = document.getElementById('school_id');
     var trackingNumberInput = document.getElementById('tracking_number');
 
     if (phoneNumberInput.value.length < phoneNumberInput.maxLength) {
         phoneNumberInput.value += value;
+        lastActiveInput = phoneNumberInput;
     } else if (trackingNumberInput.value.length < trackingNumberInput.maxLength) {
         trackingNumberInput.value += value;
+        lastActiveInput = trackingNumberInput;
     }
 }
 
 function resetForm() {
-    document.getElementById('phone_number').value = '';
+    document.getElementById('school_id').value = '';
     document.getElementById('tracking_number').value = '';
 }
 
+function backspaceForm() {
+    var phoneNumberInput = document.getElementById('school_id');
+    var trackingNumberInput = document.getElementById('tracking_number');
+
+    if (lastActiveInput != null) {
+        var inputValue = lastActiveInput.value;
+
+        if (inputValue && inputValue.length > 0) {
+            lastActiveInput.value = inputValue.slice(0, -1);
+        } else if (lastActiveInput === trackingNumberInput && inputValue.length === 0) {
+            lastActiveInput = phoneNumberInput;
+        }
+    }
+}
 
 
 document.querySelector('.hamburger-button').addEventListener('click', function () {
@@ -21,8 +39,6 @@ document.querySelector('.hamburger-button').addEventListener('click', function (
     document.querySelector('.nav-links').style.display =
         (document.querySelector('.nav-links').style.display == 'none') ? 'block' : 'none';
 });
-
-
 
 function updateZegar() {
     var today = new Date();
@@ -57,4 +73,4 @@ function updateClock() {
 }
 
 setInterval(updateClock, 1000);
-setInterval(updateZegar, 1000); 
+setInterval(updateZegar, 1000);
